3baf01a6a75b7c1a32ae96e639df26eb3576d9aa
Sanjay Manoj <sanjaymanoj04@gmail.com>
2023-06-20 18:23:18 -0500

    Created the repo
    index.html, app.py, and the virtual environment were created

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# how do i create a virtualenv 
+# a: python3 -m venv venv
+
+# how do i activate a virtualenv
+# a: source venv/bin/activate
+
+# how to deactivate a virtualenv
+# a: deactivate
+
+
+
+if __name__ == '__main__':
+    app.run()
